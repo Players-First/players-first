12c0e3564c9fb3819d7a5168146fc8e73d620739
Chewyruza <gchuacc@gmail.com>
2026-08-28 10:51:00 +0800

    Add persistent graphics shortcut

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <a className="graphics-fab" href="/graphics">Share graphics</a>
+      </body>
     </html>
   );
 }
